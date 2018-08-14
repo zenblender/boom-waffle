39c218f0abbf89279a9438284a32891b125bd7d4
Brandon Fryslie <brandon@fryslie.com>
2018-08-14 09:52:09 -0600

    build_all.sh: Use absolute paths so we can call it from any directory

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -el
 
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 for dir in shairport-sync snapcast-client snapcast-server; do
-  cd $dir
+  cd "${script_path}/${dir}"
   ./build.sh
   cd ..
 done
