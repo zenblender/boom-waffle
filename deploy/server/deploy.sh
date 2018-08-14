8fbf2dd1e8979eddb79144ac8d92c319a4463c36
Brandon Fryslie <brandon@fryslie.com>
2018-08-13 20:20:17 -0600

    Fix server deploy.sh script to remove old containers

--- deploy/server/deploy.sh
+++ deploy/server/deploy.sh
@@ -1,9 +1,13 @@
-#/bin/bash -el
+#!/bin/bash -el
+
+# We need a path entry that isn't available without this
+export PATH="$PATH:/home/pi/.local/bin"
 
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${script_path}/../../bin/_shared-scripts.sh"
 
-echo "Killing all docker containers"
-docker ps -a | awk '{print $1}' | xargs docker rm -fv
+echo "Running compose down"
+docker-compose -f "$script_path/docker-compose.yml" down
 
 echo "Starting services"
-docker-compose -f "$script_path/docker-compose.yml" up $@
+docker-compose -f "$script_path/docker-compose.yml" up -d $@
