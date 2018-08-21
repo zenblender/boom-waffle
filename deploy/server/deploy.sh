1ea16cc4a28053c13db3cc5fdd6cc0bf72119e9e
Brandon Fryslie <brandon@fryslie.com>
2018-08-20 23:17:49 -0600

    deploy.sh: also remove volumes when doing docker-compose down

--- deploy/server/deploy.sh
+++ deploy/server/deploy.sh
@@ -7,7 +7,7 @@ script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${script_path}/../../bin/_shared-scripts.sh"
 
 echo "Running compose down"
-docker-compose -f "$script_path/docker-compose.yml" down
+docker-compose -f "$script_path/docker-compose.yml" down -v
 
 echo "Starting services"
 docker-compose -f "$script_path/docker-compose.yml" up -d $@
