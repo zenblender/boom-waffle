c8ce2d13b3ae1b611e98e7cd07d3dec69b1c80b0
Brandon Fryslie <brandon@fryslie.com>
2018-08-20 23:31:00 -0600

    Remove 'privileged: true' from docker-compose.yml, it is unnecessary

--- deploy/server/deploy.sh
+++ deploy/server/deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -el
 
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${script_path}/../../bin/_shared-scripts.sh"
+
 # We need a path entry that isn't available without this
 export PATH="$PATH:/home/pi/.local/bin"
 
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${script_path}/../../bin/_shared-scripts.sh"
 
-echo "Running compose down"
+yellow "Running compose down"
 docker-compose -f "$script_path/docker-compose.yml" down -v
 
-echo "Starting services"
+yellow "Starting services"
 docker-compose -f "$script_path/docker-compose.yml" up -d $@
