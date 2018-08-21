13935806d1e5a6d0070a5a88413a78f78239ec16
Brandon Fryslie <brandon@fryslie.com>
2018-08-20 22:07:41 -0600

    use docker images from docker hub

--- docker/snapcast-server/build.sh
+++ docker/snapcast-server/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -el
 
 SNAPCAST_VERSION="0.15.0"
+image_name="brandonfryslie/snapcast-server"
 
 docker build \
-  -t snapcast-server \
+  -t "${image_name}" \
   --build-arg SNAPCAST_VERSION=${SNAPCAST_VERSION} \
   .
