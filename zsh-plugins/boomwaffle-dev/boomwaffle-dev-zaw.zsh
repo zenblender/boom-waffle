61db081101432f15163175a53305308ad425eba3
Brandon Fryslie <brandon@fryslie.com>
2018-08-20 21:25:22 -0600

    boomwaffle-dev-zaw: Add 'wait for pi' option

--- zsh-plugins/boomwaffle-dev/boomwaffle-dev-zaw.zsh
+++ zsh-plugins/boomwaffle-dev/boomwaffle-dev-zaw.zsh
@@ -18,29 +18,24 @@ function zaw-src-rad-boomwaffle-dev() {
     actions=(\
       "zaw-rad-boomwaffle-dev-ssh" \
       "zaw-rad-boomwaffle-dev-watch" \
+      "zaw-rad-boomwaffle-dev-wait-for-pi" \
       "zaw-rad-append-to-buffer" \
     )
 
     act_descriptions=(\
       "SSH to host" \
       "boom-watch (transfer files on change)" \
+      "wait for pi" \
       "append hostname to buffer"
     )
 
     options=(-t "$title" -m -k)
 }
 
-function zaw-rad-boomwaffle-dev-ssh() {
-    local hostname=$1
-    zaw-rad-buffer-action "ssh 'pi@${hostname}'"
-}
+function zaw-rad-boomwaffle-dev-ssh() { zaw-rad-buffer-action "ssh 'pi@${1}'" }
 
-function zaw-rad-boomwaffle-dev-watch() {
-    zaw-rad-buffer-action "boom-watch $1"
-}
+function zaw-rad-boomwaffle-dev-watch() { zaw-rad-buffer-action "boom-watch $1" }
 
-function zaw-rad-boomwaffle-dev-doctor() {
-    zaw-rad-buffer-action "dhost $1 && ddoctor"
-}
+function zaw-rad-boomwaffle-dev-wait-for-pi() { zaw-rad-buffer-action "waffle-wait-for-pi $1" }
 
 zaw-register-src -n rad-boomwaffle-dev zaw-src-rad-boomwaffle-dev
--- zsh-plugins/boomwaffle-dev/boomwaffle-dev.zsh
+++ zsh-plugins/boomwaffle-dev/boomwaffle-dev.zsh
@@ -4,11 +4,15 @@ export PATH="${PATH}:${BOOMWAFFLE_PATH}/bin"
 
 export BOOMWAFFLE_PATH="${BOOMWAFFLE_PATH:-"${HOME}/code/boom-waffle"}"
 
-# Add your boomwaffle ssh key to the ssh agent
+### boom-ssh - adds the boomwaffle ssh key to your agent
+### takes no arguments
 boom-ssh() {
   eval $("${BOOMWAFFLE_PATH}/bin/add-ssh.sh")
 }
 
+### boom-watch - watches the boomwaffle directory for changes and rsyncs the repo to the raspberry pi
+###
+### Example: boom-watch your-boomwaffle-hostname
 boom-watch() {
   "${BOOMWAFFLE_PATH}/bin/boom-watch.sh" "$@"
 }
@@ -33,4 +37,12 @@ boomwaffle-list() {
 
 waffle-status() {
   "${BOOMWAFFLE_PATH}/bin/waffle-status.rb" $(boomwaffle-list)
+}
+
+### waffle-wait-for-pi - calls the wait-for-pi.sh script
+### used by boomwaffle-dev-zaw
+###
+### Example: waffle-wait-for-pi your-boomwaffle-hostname
+waffle-wait-for-pi() {
+  "${BOOMWAFFLE_PATH}/bin/wait-for-pi.sh" $1
 }
