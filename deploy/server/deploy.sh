cdfa94980a7932dae32901fe98da090498073658
Brandon Fryslie <brandon@fryslie.com>
2018-08-21 23:04:25 -0600

    Add dbus volume back

--- deploy/server/deploy.sh
+++ deploy/server/deploy.sh
@@ -6,9 +6,6 @@ source "${script_path}/../../bin/_shared-scripts.sh"
 # We need a path entry that isn't available without this
 export PATH="$PATH:/home/pi/.local/bin"
 
-script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "${script_path}/../../bin/_shared-scripts.sh"
-
 yellow "Running compose down"
 docker-compose -f "$script_path/docker-compose.yml" down -v
 
