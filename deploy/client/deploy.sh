e8c77714431318276ab49db13dd23cbf4533f66d
Brandon Fryslie <brandon@fryslie.com>
2018-08-21 22:02:33 -0600

    drone / client: add deploy script and compose file

--- deploy/client/deploy.sh
+++ deploy/client/deploy.sh
@@ -1,13 +1,16 @@
-#/bin/bash -el
+#!/bin/bash -el
 
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${script_path}/../../bin/_shared-scripts.sh"
 
-docker-compose() {
-  docker run \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$PWD:/rootfs/$PWD" \
-    -w="/rootfs/$PWD" \
-    docker/compose:1.13.0 "$@"
-}
+# We need a path entry that isn't available without this
+export PATH="$PATH:/home/pi/.local/bin"
 
-docker-compose -f "$script_path/docker-compose.yml"
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${script_path}/../../bin/_shared-scripts.sh"
+
+yellow "Running compose down"
+docker-compose -f "$script_path/docker-compose.yml" down -v
+
+yellow "Starting client services"
+docker-compose -f "$script_path/docker-compose.yml" up -d $@
