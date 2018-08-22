32328b6aed0f2c18b27244437374928571b23436
Brandon Fryslie <brandon@fryslie.com>
2018-08-21 22:06:27 -0600

    drone: Add avahi-utils to the Dockerfile and resolve the hostname with avahi if necessary

--- docker/snapcast-client/files/start.sh
+++ docker/snapcast-client/files/start.sh
@@ -1,3 +1,19 @@
 #!/bin/bash -el
 
-snapclient --soundcard $SOUNDCARD_ID -h $SNAPCAST_SERVER_HOSTNAME
+# Setting this to 24 allows it to use the USB sound card when it is plugged in,
+# otherwise it will use onboard sound
+SOUNDCARD_ID=24
+
+[[ -z "${SNAPCAST_SERVER_HOSTNAME}" ]] && { echo 'Error: Must set $SNAPCAST_SERVER_HOSTNAME'; exit 1; }
+
+# If the environment variable USE_AVAHI is set, we will use avahi to resolve the hostname
+if [[ "${USE_AVAHI}" == 'true' ]]; then
+  hub_address="$(avahi-resolve-host-name -4 "${SNAPCAST_SERVER_HOSTNAME}" | awk '{print $2}')"
+else
+  hub_address="${SNAPCAST_SERVER_HOSTNAME}"
+fi
+
+echo "Starting snapcast client with hub: ${SNAPCAST_SERVER_HOSTNAME}"
+[[ "${USE_AVAHI}" == 'true' ]] && echo "Address resolved by avahi: ${hub_address}"
+
+snapclient --soundcard "${SOUNDCARD_ID}" -h "${hub_address}"
